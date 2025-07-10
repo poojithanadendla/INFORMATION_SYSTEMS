a5fbddc4335ccfa2de61a258b08071be954d72b1
Gnana Poojitha Nadendla <109503002+poojithanadendla@users.noreply.github.com>
2025-07-10 13:10:37 +0100

    loading and saving data

--- app.py
+++ app.py
@@ -4,4 +4,16 @@ import json
 import os
 app = Flask(__name__, template_folder='templates')
 CORS(app)
+
 DATA_FILE = 'backend/developers.json'
+
+def load_data():
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, 'r') as F:
+        return json.load(F)
+
+def save_data(data):
+    with open(DATA_FILE, 'w') as f:
+        json.dump(data, f, indent=4)
+
